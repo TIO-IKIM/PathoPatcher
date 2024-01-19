1a65dfd98046fa97cf7ac9677e6be2fafde8df9d
Fabian Hörst <fabian.hoerst@rub.de>
2024-01-19 10:14:53 +0100

    Changing CLI

--- pathopatch/wsi_extraction.py
+++ pathopatch/wsi_extraction.py
@@ -10,10 +10,10 @@
 import sys
 import os
 
-# project_root = os.path.dirname(os.path.abspath(__file__))
-# sys.path.append(project_root)
-# project_root = os.path.dirname(os.path.abspath(project_root))
-# sys.path.append(project_root)
+project_root = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(project_root)
+project_root = os.path.dirname(os.path.abspath(project_root))
+sys.path.append(project_root)
 
 import logging
 
